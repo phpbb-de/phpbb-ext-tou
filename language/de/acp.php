a6e31dda6f724878b65423e82e034a27b3741b11
Christian Schnegelberger <mail@crizzo.de>
2020-09-11 13:38:32 +0200

    Add german translation and improve english version

--- language/de/acp.php
+++ language/de/acp.php
@@ -32,7 +32,37 @@ $lang = array_merge($lang, array(
 
 	'ACP_TOU_SETTINGS_UPDATED'			=> 'Die Einstellung von „Terms of Use“ wurden aktualisiert.',
 
-	// Custom TOU and PP setup pages
-	'ACP_TOU_TOU_UPDATED'			=> 'Die „Nutzungsbedingungen“ wurden aktualisiert.',
-	'ACP_TOU_PP_UPDATED'			=> 'Die „Datenschutzerklärung“ wurde aktualisiert.',
+	// Custom TOU setup
+	'ACP_TOU_TOU_UPDATED'					=> 'Die „Nutzungsbedingungen“ wurden aktualisiert.',
+
+	'ACP_TOU_TOUSETUP_TITLE'				=> 'Eigene „Nutzungsbedingungen“ einrichten',
+	'ACP_TOU_TOUSETUP_TITLE_EXPLAIN'		=> 'Die mit phpBB mitgelieferten „Nutzungsbedingungen“ sind allgemein gehalten und sollten von dir für deine Bedürfnisse und Board-Konfiguration angepasst werden.',
+
+	'ACP_TOU_TOUSETUP_FIELDSET'				=> 'Einstellungen',
+	'ACP_TOU_TOUSETUP_FIELDSET_EXPLAIN'		=> 'Ändere verschiedene Einstellungen für die „Nutzungsbedingungen“.',
+
+	'ACP_TOU_INFO_PREVIEW_TOU' 				=> '„Nutzungsbedingungen“-Vorschau',
+
+	'ACP_TOU_TOUSETUP_USE_CUSTOM'			=> 'Eigene „Nutzungsbedingungen“ verwenden',
+	'ACP_TOU_TOUSETUP_USE_CUSTOM_EXPLAIN'	=> 'Sofern aktiviert, werden deine eigenen „Nutzungsbedingungen“ anstelle der original phpBB-Version verwendet.',
+
+	'ACP_TOU_TEXT_INPUT_TOU'				=> '„Nutzungsbedingungen“-Text ergänzen',
+	'ACP_TOU_TEXT_INPUT_TOU_EXPLAIN'		=> 'Trage deine eigenen „Nutzungsbedingungen“ hier in das Textfeld ein.',
+
+	// Custom PP setup
+	'ACP_TOU_PP_UPDATED'					=> 'Die „Datenschutzerklärung“ wurde aktualisiert.',
+
+	'ACP_TOU_PPSETUP_TITLE'					=> 'Eigene „Datenschutzerklärung“ einrichten',
+	'ACP_TOU_PPSETUP_TITLE_EXPLAIN'			=> 'Die mit phpBB mitgelieferte „Datenschutzerklärung“ ist allgemein gehalten und sollte von dir für deine Bedürfnisse und Board-Konfiguration angepasst werden.',
+
+	'ACP_TOU_PPSETUP_FIELDSET'				=> 'Einstellungen',
+	'ACP_TOU_PPSETUP_FIELDSET_EXPLAIN'		=> 'Ändere verschiedene Einstellungen für die „Datenschutzerklärung“.',
+
+	'ACP_TOU_INFO_PREVIEW_PP' 				=> '„Datenschutzerklärung“-Vorschau',
+
+	'ACP_TOU_PPSETUP_USE_CUSTOM'			=> 'Eigene „Datenschutzerklärung“ verwenden',
+	'ACP_TOU_PPSETUP_USE_CUSTOM_EXPLAIN'	=> 'Sofern aktiviert, wird deine eigene „Datenschutzerklärung“ anstelle der original phpBB-Version verwendet.',
+
+	'ACP_TOU_TEXT_INPUT_PP'					=> '„Datenschutzerklärung“-Text ergänzen',
+	'ACP_TOU_TEXT_INPUT_PP_EXPLAIN'			=> 'Trage deine eigene „Datenschutzerklärung“ hier in das Textfeld ein.',
 ));
