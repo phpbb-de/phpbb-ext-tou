5e4e393be4285fe8e26fe09c2931485e759e43b6
Christian Schnegelberger <mail@crizzo.de>
2020-10-04 13:57:42 +0200

    Update version to 1.1.4

---
+++ migrations/v114.php
@@ -0,0 +1,30 @@
+<?php
+/**
+*
+* Terms of use extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2020 Crizzo <https://www.phpBB.de>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace phpbbde\tou\migrations;
+
+class v114 extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array(
+			'\phpbbde\tou\migrations\v113',
+		);
+	}
+
+	public function update_data()
+	{
+		$data = array(
+			// Update version
+			array('config.update', array('tou_ext_version', '1.1.4')),
+		);
+		return $data;
+	}
+}
